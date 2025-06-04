bdbac0bc9c2280f9afe29f18eefb940e6a47bc6b
Pushkar Singh <rbt4209211@gmail.com>
2025-06-04 11:10:04 +0530

    created methods in userSchema  for hashing password and generating access and refresh tokens using jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new Schema({
         lowercase: true,
         trim: true
     },
-    fullname: {
+    fullName: {
         type: String,
         required: true,
         trim: true,
@@ -45,6 +45,45 @@ const userSchema = new Schema({
     },
 
 },{ timestamps: true})
+
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password, 10 )
+        next()
+    }
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.envv.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.envv.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 export const User = mongoose.model("User", userSchema)
